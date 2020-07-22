b699f9c5741e24cb2856f4194f8a9512e8ba629c
michelle <foleymichelle9@gmail.com>
2020-07-21 20:17:10 -0500

    Add madelines routes file

---
+++ spec/welcome/home_page_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe "Welcome Page" do
+  it "should have landing page, log in link and todays neos" do
+    visit "/"
+
+    expect(page).to have_content("WELCOME TO HORROR SCOPES")
+    expect(page).to have_content("TODAYS TRAGEDY AS PREDICTED BY THE PLANETS")
+    expect(page).to have_css("img[src*='cathulu_vector.jpg']")
+    expect(page).to have_content("project statement and intent")
+  end
+end

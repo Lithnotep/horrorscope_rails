570b1f28cb1c90e943d689b618ad6f2ff32fc2b7
Max Mitrani <mjmitrani168@gmail.com>
2020-07-24 15:03:09 -0600

    should matchers added to rails helper .rb

--- spec/models/daily_harbinger_spec.rb
+++ spec/models/daily_harbinger_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe DailyHarbinger, type: :model do
   describe 'Relationships' do
-    it {should belong_to(:daily_message)}
-    it {should belong_to(:harbinger)}
+    it {should belong_to :daily_message}
+    it {should belong_to :harbinger}
   end
 end

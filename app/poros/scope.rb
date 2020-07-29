1f4b089aa075569419fd45d09ff9cfde5ea11ad6
Max Mitrani <mjmitrani168@gmail.com>
2020-07-29 17:02:17 -0600

    add more scopes data

--- app/poros/scope.rb
+++ app/poros/scope.rb
@@ -1,15 +1,16 @@
 class Scope
 
   def initialize
-    @description = "Your likely to be #{doom} by a #{thing} leading to a #{death} death on #{date}"
+    @description = "Your likely to be #{doom} by a #{adj+thing} leading to a #{death} death on #{date}"
   end
 
   def doom
-    dooms = ["devoured", eaten alive, evisorated]
+    dooms = ["devoured", "eaten alive", "eviscerated", "choked", "warped", "corrupted", "exploded", "imploded", "deconstructed", 'disemboweled', "petrified", "made ephemeral", "made unreal", "dreamed away", "negated"]
+    dooms.sample
   end
 
   def thing
-
+    things = ["Star-Child of Lost Kadath", "Bear", "Spawn of Darkness", ""]
   end
 
   def death
@@ -17,6 +18,13 @@ class Scope
   end
 
   def date
+    year = [2020..2100].sample
+    month = [1..12].sample
+    day = [1..28].sample
+    "#{year}-#{month}-#{day}"
+  end
+
+  def adj
 
   end
 

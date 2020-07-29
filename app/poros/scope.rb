b243be91c97eb3a73f0a4b0af47dc8f5012ea1dd
Max Mitrani <mjmitrani168@gmail.com>
2020-07-29 15:06:54 -0600

    add scope pror

--- app/poros/scope.rb
+++ app/poros/scope.rb
@@ -1,13 +1,23 @@
 class Scope
 
   def initialize
-    @description = "You will be #{doom} by a #{thing} leading to a #{death} death on #{date}"
+    @description = "Your likely to be #{doom} by a #{thing} leading to a #{death} death on #{date}"
   end
 
   def doom
-
+    dooms = ["devoured", eaten alive, evisorated]
   end
 
   def thing
+
   end
+
+  def death
+
+  end
+
+  def date
+
+  end
+
 end

1af177853b6f2821e377c3a838aa6d6783174576
Max Mitrani <mjmitrani168@gmail.com>
2020-07-29 15:08:56 -0600

    daily merge

--- spec/features/visitors/daily_spec.rb
+++ spec/features/visitors/daily_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe 'visitor can visit daily horrorscope page' do
   it 'and see what possibilities await' do
     visit '/daily'
-    save_and_open_page
 
     expect(page).to have_content('DAILY DARKNESS')
     expect(page).to have_content('FOUR HARBINGERS')

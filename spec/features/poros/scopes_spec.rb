16232f3c63a484b7b06f179869cadfffe9a5f952
Max Mitrani <mjmitrani168@gmail.com>
2020-07-29 20:45:10 -0600

    remove pry

--- spec/features/poros/scopes_spec.rb
+++ spec/features/poros/scopes_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "scope can generate" do
   it "makes a scope" do
     scope = Scope.new
-    binding.pry
+
     scope.description.should an_instance_of(String)
   end
 end
